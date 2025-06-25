e6e062c2158252690f9f6492bfb897e57e6c12e3
DeltaMonkey <engincanozkan@outlook.com>
2025-06-25 14:27:40 +0300

    High Score persistence

--- Globals/ScoreManager.cs
+++ Globals/ScoreManager.cs
@@ -5,20 +5,32 @@ public partial class ScoreManager : Node
 {
 	public static ScoreManager Instance { get; private set; }	
 
-	private int _score { get; set; } = 0;// the game score
-	private int _highScore { get; set; } = 0; // the high score
+	private uint _score { get; set; } = 0;// the game score
+	private uint _highScore { get; set; } = 0; // the high score
+
+	private const string SCORE_FILE = "user://tappy.save";
+	/*
+		.txt
+		.dat
+		.bin
+	*/
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Instance = this;
-
-		ResetScore();
+		LoadScoreFromFile();
 	}
 
-	public static int GetScore() => Instance._score; // return the score
-	public static int GetHighScore() => Instance._highScore; // return the highScore
-	public static void SetScore(int score) // set the score
+    public override void _ExitTree()
+    {
+		SaveScoreToFile();
+    }
+
+	public static uint GetScore() => Instance._score; // return the score
+	public static uint GetHighScore() => Instance._highScore; // return the highScore
+
+	public static void SetScore(uint score) // set the score
 	{
 		Instance._score = score;
 		if(Instance._score > Instance._highScore) 
@@ -28,10 +40,31 @@ public partial class ScoreManager : Node
 		GD.Print($"Score: {Instance._score}, High Score: {Instance._highScore}");
 		SignalManager.EmitOnScored();
 	}
+
 	public static void ResetScore() // reset score to 0
 	{
 		SetScore(0);		
 	}
+
 	public static void IncrementScore() => SetScore(GetScore() + 1);// increment score by 1
+
+	private void SaveScoreToFile()
+	{
+		using FileAccess file = FileAccess.Open(SCORE_FILE, FileAccess.ModeFlags.Write);
+		if(file != null)
+		{
+			// file.StoreString(_highScore.ToString());
+			file.Store32(_highScore);
+		}
+	}
+
+	private void LoadScoreFromFile()
+	{
+		using FileAccess file = FileAccess.Open(SCORE_FILE, FileAccess.ModeFlags.Read);
+		if(file != null)
+		{
+			_highScore = file.Get32();
+		}
+	}
 
 }
